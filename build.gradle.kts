a01ebf7f09f979a262c5414514d93ad403fa45a4
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-03-03 15:16:18 +0100

    Simplify build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,8 @@ buildscript {
     }
 }
 
-// Temporary fix for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
-@Suppress(
-    "DSL_SCOPE_VIOLATION",
-    "MISSING_DEPENDENCY_CLASS",
-    "UNRESOLVED_REFERENCE_WRONG_RECEIVER",
-    "FUNCTION_CALL_EXPECTED"
-)
+// Workaround for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     java
     jacoco
--- kode/build.gradle.kts
+++ kode/build.gradle.kts
@@ -1,10 +1,5 @@
-// Temporary fix for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
-@Suppress(
-    "DSL_SCOPE_VIOLATION",
-    "MISSING_DEPENDENCY_CLASS",
-    "UNRESOLVED_REFERENCE_WRONG_RECEIVER",
-    "FUNCTION_CALL_EXPECTED"
-)
+// Workaround for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     `java-library`
     alias(libs.plugins.kotlin.jvm)
@@ -19,6 +14,7 @@ dependencies {
 
     // Testing
     testImplementation  (libs.kotlin.reflect)
+    testImplementation  (libs.kotest)
     testImplementation  (libs.junit)
     testImplementation  (libs.awaitility.core)
     testImplementation  (libs.awaitility.kotlin)
